9a33a25aa197cbd98bf3c9f1f1e8d8d499d775b6
liang0428 <liang.cc@hotmail.com>
2015-11-03 15:02:45 -0500

    0302pm

--- +PhyNet/@Macro/Macro.m
+++ +PhyNet/@Macro/Macro.m
@@ -2,7 +2,7 @@ classdef Macro <PhyNet.BaseStation
 %
     %Properties
     properties (SetAccess='private') % Private parameters
-        radius
+        radius %Radius
         attatchedPicoList
         attatchedSmallList
     end
@@ -12,6 +12,7 @@ classdef Macro <PhyNet.BaseStation
        function  this = Macro(val1,val2) % Constructor: initialize id, position, and radius
                 this@PhyNet.BaseStation(val1)
                 this.radius = val2;
+                this.position=this.radius.*MacroCellLocation(this.id);
        end
     end
 
