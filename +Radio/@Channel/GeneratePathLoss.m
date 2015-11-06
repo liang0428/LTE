9c84ab781f24742e3801328dd3e444bc7374a019
liang0428 <liang.cc@hotmail.com>
2015-11-06 14:34:35 -0500

    Gitignore

---
+++ +Radio/@Channel/GeneratePathLoss.m
@@ -0,0 +1,23 @@
+function this=GeneratePathLoss(this,arg1)
+
+%PathLoss is used to calculate the pathloss of a channel object 'this'
+%arg is used to select the pathloss model
+%Results return to this.pathLoass units in dB
+
+
+d=pdist2(this.rxPoint.position,this.txPoint.position,'Euclidean'); %Distance (in Meters) between Rx and Tx
+
+switch arg1
+    case 'Macro'
+        PL=34 + 40* log10(d);
+    case 'Small'
+        PL=37 + 30 *log10(d);
+    otherwise
+end
+
+this.pathLoss=PL;
+
+% switch 
+
+    
+end
--- +Radio/@Channel/GenerateShadowing.m
+++ +Radio/@Channel/GenerateShadowing.m
@@ -1,22 +1,19 @@
-function this=PathLoss(this,arg1,arg2)
+function this=GenerateShadowing(this,arg1)
 
 %PathLoss is used to calculate the pathloss of a channel object 'this'
 %arg is used to select the pathloss model
 %Results return to this.pathLoass units in dB
 
 
-d=pdist2(this.rxPoint.position,this.txPoint.position,'Euclidean'); %Distance (in Meters) between Rx and Tx
-
 switch arg1
-
     case 'Macro'
-        PL=34 + 40* log10(d);
+        SD=normrnd(0,8);
     case 'Small'
-        PL=37 + 30 *log10(d);
+        SD=normrnd(0,4);
     otherwise
-        ;
 end
 
-switch 
+this.pathLoss=SD;
 
+    
 end
